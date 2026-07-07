8c2252e9e72aa9899249f372680193c9ee718f14
Morgane Le Fustec <morgane.lefustec@outlook.fr>
2026-07-07 16:25:16 +0200

    feat: adding the GET route for travel

--- backend/src/routes/travel.js
+++ backend/src/routes/travel.js
@@ -2,21 +2,21 @@ import express from 'express'
 import { Pool } from 'pg'
 import cors from 'cors'
 
-const app = express()
+const travelRouter = express.Router()
 const port = 3000
 
 const pool = new Pool({
     user : "postgres",
-    host : "localhost",
-    database : "adatabase",
+    host : "database",
+    database : "globetrotteuse",
     password : "postgres",
     port : "5432"
 })
 
-app.use(express.json())
-app.use(cors())
+travelRouter.use(express.json())
+travelRouter.use(cors()) 
 
-app.get('/check-travel', async (req, res)=> {
+travelRouter.get('/check-travel', async (req, res)=> {
     const titre = req.body
     if (!titre) {
         return res.status(400).json({ error : 'Titre manquant'})
@@ -33,6 +33,5 @@ app.get('/check-travel', async (req, res)=> {
 
 // })
 
-app.listen(port, () => {
-    console.log("serveur Les Globetrotteuses lancé")
-})
+
+export default travelRouter
