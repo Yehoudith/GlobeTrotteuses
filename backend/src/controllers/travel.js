a200c77b772a61e676c22b90132df730705fde8f
Morgane Le Fustec <morgane.lefustec@outlook.fr>
2026-07-20 18:17:03 +0200

    feat: route for travel creation done in backend

--- backend/src/controllers/travel.js
+++ backend/src/controllers/travel.js
@@ -1,25 +1,31 @@
 import { db } from '../../db.js';
-import { findTravel } from '../models/travel.js';
+import { createNameTravel, findTravel } from '../models/travel.js';
 import { checkNameTravel } from '../services/travel.js';
 
 export const checkTravel = async (req,res) => {
     const { title } = req.body || {}
     if (!title) {
-     return res.status(400).json({ error : 'Titre manquant'})
+     return {valid: false,  error : 'Titre manquant'}
     }
     const response = await checkNameTravel(title)
     if (!response){
-        return res.status(400).json({error : 'Ce nom de voyage est déjà pris'})
-    } else {
-        res.send('ok')
-    }   
+        return {valid: false, error : 'Ce nom de voyage est déjà pris'}
+    }
+    return {valid: true}
 }
 
 export const createTravel = async (req, res) => {
-    const result = await checkTravel(req,res)
-    console.log(result)
-    // if (result.res == 400) 
-    //     // à vérifier
-    // const { title } = req.body || {}
-    // createNameTravel
+    try {
+        const { title, starting_date, ending_date } = req.body || {}
+        const check = await checkTravel(req,res)
+        if (!check.valid) {
+            return res.status(400).json({error : check.error})
+        } 
+        const response = await createNameTravel(title, starting_date, ending_date)
+        return res.status(201).json(response[0])
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Erreur serveur'})
+    }
 }
